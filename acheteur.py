8be6e979740fefee052c04673a02cd84b33b57bb
ludivineUp <44409644+ludivineUp@users.noreply.github.com>
2023-11-10 14:31:52 +0100

    Modif offre en liste

---
+++ acheteur.py
@@ -0,0 +1,5 @@
+class Acheteur():
+
+	def __init__(self, nom):
+		self.name = nom
+		self.offre = []
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from acheteur import Acheteur
 from objet import Objet
 
+# Le prix gagnant est le prix de l'offre la plus élevée d'un acheteur 
+# non gagnant au-dessus du prix de réserve 
+# (ou le prix de réserve s'il n'y en a aucun qui s'applique)
+
+def winner(enchere, acheteurs):
+
 def createAcheteurs(nb):
 	acheteurs = []
 	for i in range(nb):
@@ -10,7 +16,7 @@ def createAcheteurs(nb):
 
 def saisieOffre(acheteurs):
 	for a in acheteurs:
-		a.offre = int(input("Achteur", a.name, "entrer votre offrek"))
+		a.offre.append(int(input("Acheteur", a.name, "entrer votre offre")))
 
 def tour(n, acheteurs):
 	for i in range(n):
@@ -21,7 +27,7 @@ def main():
 	name-objet = input("Enter l'objet")
 	prix-objet = input("Enter le prix de l'objet")
 	enchere = Objet(name-objet, prix-objet)
-	
+
 	nb-acheteurs = int(input("Combien d'achteurs ?"))
 	acheteurs = createAcheteurs(nb-acheteurs)
 	tours_achat(3, acheteurs)
